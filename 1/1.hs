6973f4aec0667186315f2f668a0781f462a54d81
Job Vonk <sticcpistt@icloud.com>
2022-01-24 16:01:14 +0100

    Made 1.hs even shorter, because I couldn't stand it

--- 1/1.hs
+++ 1/1.hs
@@ -1,8 +1,7 @@
-part1 = print . length . filter (\(a, b) -> a < b) . (zip <*> tail)
+part1 = print . length . filter (uncurry (<)) . (zip <*> tail)
 part2 = part1 . createTriplets
   where createTriplets (a : b : c : ds) = a + b + c : (createTriplets $ b : c : ds)
         createTriplets _ = []
 main  = do
-  s <- readFile "1.txt"
-  let ns = (map read :: [String] -> [Int]) . words $ s
+  ns <- readFile "1.txt" >>= return . map read . words
   part1 ns ; part2 ns
