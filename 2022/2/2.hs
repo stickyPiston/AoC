2a0ef444f5265ddd196cd89613fd465b4e722661
Job Vonk <sticcpistt@icloud.com>
2022-12-06 19:27:45 +0100

    general code cleanup

--- 2022/2/2.hs
+++ 2022/2/2.hs
@@ -1,9 +1,6 @@
 import Data.Char
 
-myscore :: Char -> Int
 myscore c = ord c - 87
-
-win :: Char -> Char -> Int
 win 'B' 'X' = 0
 win 'C' 'X' = 6
 win 'A' 'Y' = 6
@@ -11,11 +8,8 @@ win 'C' 'Y' = 0
 win 'A' 'Z' = 0
 win 'B' 'Z' = 6
 win opp me | ord opp - 65 == ord me - 88 = 3
-
-part1 :: [[Char]] -> Int
 part1 guide = sum [win opp me + myscore me | [opp, me] <- guide] 
 
-strategy :: Char -> Char -> Int
 strategy 'A' 'X' = 3
 strategy 'B' 'X' = 1
 strategy 'C' 'X' = 2
@@ -25,11 +19,8 @@ strategy 'C' 'Y' = 3 + 3
 strategy 'A' 'Z' = 2 + 6
 strategy 'B' 'Z' = 3 + 6
 strategy 'C' 'Z' = 1 + 6
-
-part2 :: [[Char]] -> Int
 part2 guide = sum [strategy opp me | [opp, me] <- guide]
 
-main :: IO ()
 main = do
   guide <- map (map head . words) . lines <$> readFile "2.txt"
   putStrLn $ "Part 1: " ++ show (part1 guide)
