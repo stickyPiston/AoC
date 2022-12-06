93c9297eb0d03effef206438b1e41bb15bc29437
Job Vonk <sticcpistt@icloud.com>
2022-12-06 19:22:42 +0100

    cleaned up 2022 1

--- 2022/1/1.hs
+++ 2022/1/1.hs
@@ -1,15 +1,9 @@
 import Data.List (sort)
-
-split :: Eq a => a -> [a] -> [[a]]
-split delim xs = go xs [] where
-  go [] acc = [acc]
-  go (x : xs) acc | x == delim = acc : go xs []
-                  | otherwise  = go xs (x : acc)
+import Data.List.Split
 
 main = do
   content <- lines <$> readFile "1.txt"
-  let total_calories = [ sum $ map read elf
-                       | elf <- split "" content]
+  let total_calories = [sum $ map read elf | elf <- splitOn [""] content]
   putStrLn $ "Part one: " ++ show (maximum total_calories)
   let top_three = take 3 $ reverse $ sort total_calories
   putStrLn $ "Part two: " ++ show (sum top_three)
